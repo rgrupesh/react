4c94d49659848042e5972d822d2f081d226ff5ca
Rupesh Gelal <rupeshgelal9@gmail.com>
2020-10-07 15:45:46 +0545

    destructing Props

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
-const Cat = () => {
+const Cat = ({name,color,type}) => {
     return React.createElement(
         "div",
         {},
-        React.createElement("h1",{},"Luna"),
-        React.createElement("h2",{},"Black"),
-        React.createElement("h2",{},"Sweet"),
+        React.createElement("h1",{},name),
+        React.createElement("h2",{},color),
+        React.createElement("h2",{},type),
         );
 };
 
-const Dog = () => {
+const Dog = ({type, look}) => {
     return React.createElement(
         "div",
         {},
-        React.createElement("h2",{}, "Husky"),
-        React.createElement("h2",{}, "Foxy"),
+        React.createElement("h1",{}, type),
+        React.createElement("h2",{}, look),
     );
 };
 
@@ -22,8 +22,15 @@ const App = () => {
         "div",
         {},
         React.createElement("h1",{},"Hello World"),
-        React.createElement(Cat),
-        React.createElement(Dog)
+        React.createElement(Cat, {
+            name : "Luna",
+            color: "Black",
+            type : "Sweet",
+        }),
+        React.createElement(Dog,{
+            type: "Sheperd",
+            look : "Foxy",
+        })
     );
 };
 
