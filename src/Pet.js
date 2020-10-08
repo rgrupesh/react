bc346056b86a71fd4214d954abcf6e573a682506
Rupesh Gelal <rupeshgelal9@gmail.com>
2020-10-08 12:52:03 +0545

    eslint config

--- src/Pet.js
+++ src/Pet.js
@@ -1,11 +1,11 @@
 import React from "react";
 
 export default function Pet({ name, color, type }) {
-  return React.createElement(
-    "div",
-    {},
-    React.createElement("h1", {}, name),
-    React.createElement("h2", {}, color),
-    React.createElement("h2", {}, type)
+  return (
+    <div>
+      <h1>{name}</h1>
+      <h2>{color}</h2>
+      <h3>{type}</h3>
+    </div>
   );
 }
