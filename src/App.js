6dcfb7a99af964d0bab2b7eab938396d08cbe971
Rupesh Gelal <rupeshgelal9@gmail.com>
2020-10-08 13:04:26 +0545

    jsx

--- src/App.js
+++ src/App.js
@@ -3,16 +3,22 @@ import { render } from "react-dom";
 import Pet from "./Pet";
 
 const App = () => {
-  return React.createElement(
-    "div",
-    {},
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, {
-      name: "Luna",
-      color: "Black",
-      type: "Sweet",
-    })
+  // return React.createElement(
+  //   "div",
+  //   {},
+  //   React.createElement("h1", {}, "Adopt Me!"),
+  //   React.createElement(Pet, {
+  //     name: "Luna",
+  //     color: "Black",
+  //     type: "Sweet",
+  //   })
+  // );
+  return (
+    <div>
+      <h1 id="new id">Adopt Me!</h1>
+      <Pet name="Luna" color="Black" type="sweet" />
+    </div>
   );
 };
 
-render(React.createElement(App), document.getElementById("root"));
+render(<App />, document.getElementById("root"));
--- src/Pet.js
+++ src/Pet.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Pet({ name, color, type }) {
   return (
     <div>
-      <h1>{name}</h1>
+      <h1>Name:{name}</h1>
       <h2>{color}</h2>
       <h3>{type}</h3>
     </div>
